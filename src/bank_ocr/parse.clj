a137ede9e5703e9263c061d795f9aed3f6256410
Stephen Caudill <scaudill@gmail.com>
2014-06-25 10:45:35 -0600

    Transform rows vector to char matrices

--- src/bank_ocr/parse.clj
+++ src/bank_ocr/parse.clj
@@ -3,3 +3,6 @@
 (defn parse [ocr-text]
   (map #(re-seq #"." %)
        (clojure.string/split ocr-text #"\n")))
+
+(defn matrices [parsed]
+  (apply mapv vector (map #(partition 3 %) parsed)))
--- test/bank_ocr/parse_test.clj
+++ test/bank_ocr/parse_test.clj
@@ -4,5 +4,13 @@
 
 (def zero-to-nine (slurp "fixtures/one_to_nine.txt"))
 
+;; parse
+;; verifying the shape is 3x27; 3 rows of 27 characters
 (is (= 3 (count (parse zero-to-nine))))
 (is (= 27 (count (first (parse zero-to-nine)))))
+
+;; matrices
+;; verifying the shape is 9x3x3; 9 3x3 character matrices
+(is (= 9 (count (matrices (parse zero-to-nine)))))
+(is (= 3 (count (first (matrices (parse zero-to-nine))))))
+(is (= 3 (count (first (first (matrices (parse zero-to-nine)))))))
